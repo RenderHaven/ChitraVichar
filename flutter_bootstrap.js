9805564cf8ab63d744524c350c5dc6425e28e074
RenderHaven <vikrambalai1002@gmail.com>
2025-03-16 02:42:17 +0530

    Again2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3285261978"
+    serviceWorkerVersion: "1983233610"
   }
 });
